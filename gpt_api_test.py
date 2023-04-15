9279fd738de0590f15d4ed3c0c745fa1580a65f8
munir-v <81745551+munir-v@users.noreply.github.com>
2023-04-14 20:18:37 -0700

    first commit

---
+++ gpt_api_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import openai
+import constants
+
+# Set up the OpenAI API client
+api_key = constants.GPT_API_KEY
+openai.api_key = api_key
+
+def ask_gpt(question):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"{question}\nAnswer:",
+        temperature=0.7,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    answer = response.choices[0].text.strip()
+    return answer
+
+
+# question = str(os.environ["KMVAR_GPTprompt"])
+# answer = ask_gpt(question)
+# print(answer)
+
+if __name__ == "__main__":
+    question = input("Please enter your question: ")
+    answer = ask_gpt(question)
+    print(f"Answer: {answer}")
